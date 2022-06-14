dab6c3d2fd944127fc5b6fe4212cf0f43e9b993d
Frazonar <snip.coloured@gmail.com>
2022-06-14 13:18:07 +0300

    Initial commit

---
+++ react-ts-sass/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "prefer-const": 1, // make prefer-const a warning instead of an error
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+};
---
+++ react-ts/README.md
@@ -0,0 +1,18 @@
+# Vite React Template
+
+## Usage:
+
+```bash
+$ yarn # or npm install
+```
+
+See `package.json` for a list of available scripts.
+
+## Packages included:
+
+- Vite
+- React
+- TypeScript
+- Jest
+- ESlint
+- Prettier
