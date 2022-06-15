92c2126a29ba36d1a36806b42b3f74194ab33e08
Frazonar <snip.coloured@gmail.com>
2022-06-15 14:48:18 +0300

    solid-ts: add template

---
+++ solid-ts/src/__tests__/App.test.tsx
@@ -0,0 +1,24 @@
+import { render, fireEvent, screen } from 'solid-testing-library';
+import App from "../App";
+
+test("Renders main page correctly", async () => {
+  // Setup
+  render(() => <App />);
+  const buttonCount = await screen.findByRole("button");
+  let codeCount = await screen.queryByText(/The count is now:/);
+
+  // Pre-expectations
+  expect(buttonCount.innerHTML).toBe("count is: 0");
+  expect(codeCount).not.toBeInTheDocument();
+
+  // Init
+  fireEvent.click(buttonCount);
+  await Promise.resolve();
+  fireEvent.click(buttonCount);
+  await Promise.resolve();
+  codeCount = await screen.queryByText(/The count is now:/);
+
+  // Post-expectations
+  expect(buttonCount.innerHTML).toBe("count is: 2");
+  expect(codeCount).toBeInTheDocument();
+});
