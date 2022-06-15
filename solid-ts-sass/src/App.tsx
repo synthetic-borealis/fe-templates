bb512ab4f3a6726fef35ca9fc9c3d39d6d091da0
Frazonar <snip.coloured@gmail.com>
2022-06-15 15:20:15 +0300

    solid-ts-sass: add template

---
+++ solid-ts-sass/src/App.tsx
@@ -0,0 +1,51 @@
+import { Component, createSignal, Show } from "solid-js";
+
+import logo from "./logo.svg";
+import "./App.scss";
+
+const App: Component = () => {
+  const [count, setCount] = createSignal<number>(0);
+
+  return (
+    <div class="App">
+      <header class="App-header">
+        <img src={logo} class="App-logo" alt="logo" />
+        <p>Hello Vite + Solid!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count()}
+          </button>
+        </p>
+        <Show when={count() > 0}>
+          <p>
+            <code>The count is now: {count()}</code>
+          </p>
+        </Show>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            class="App-link"
+            href="https://github.com/solidjs/solid"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Solid
+          </a>
+          {" | "}
+          <a
+            class="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
